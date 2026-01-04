fe24d248deae0d81f3b3b549f781f6a799979865
GProko <gproko.26@gmail.com>
2026-01-04 11:22:15 +0530

    Basic Running Server Setup Done

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const sqiRoutes = require('./routes/sqiRoutes');
 const app = express();
 const PORT = 5000;
 
+// Middleware
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' }));
 
+// Routes
+app.use('/api', sqiRoutes);
 
 // Test
 app.get('/', (req, res) => {
