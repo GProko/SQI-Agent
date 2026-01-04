94ce86ac0a4a1f1c53f06c58047f6db934ffc866
GProko <gproko.26@gmail.com>
2026-01-04 11:45:00 +0530

    sqi model

---
+++ server/models/sqiModel.js
@@ -0,0 +1,9 @@
+const IMPORTANCE = { 'A': 1.0, 'B': 0.7, 'C': 0.5 };
+const DIFFICULTY = { 'Easy': 0.6, 'Medium': 1.0, 'Hard': 1.4 };
+const TYPE_W = { 'Practical': 1.1, 'Theory': 1.0 };
+
+const calculateSQI = (attempts) => {
+      
+     
+
+}
