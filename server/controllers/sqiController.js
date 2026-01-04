47b2f376df9ad3d4450a2342c940c84c075d8312
GProko <gproko.26@gmail.com>
2026-01-04 11:33:09 +0530

    controller setup

---
+++ server/controllers/sqiController.js
@@ -0,0 +1,12 @@
+// controllers/sqiController.js
+
+
+exports.computeSQI = (req, res) => {
+    try {
+       
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500);
+    }
+};
